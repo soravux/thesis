7e5bb16bf5f3406a05d81b593eb6fb551f461882
Yannick Hold <yannickhold@gmail.com>
2018-03-30 21:51:14 -0400

    Removed unused files

--- annex5_figures/horizon_estimation/thumb/blu.py
+++
@@ -1,9 +0,0 @@
-import os
-from glob import glob
-
-imgs = glob("*.jpg")
-
-for im in imgs:
-    if not os.path.isfile(os.path.join("..", im)):
-        print("unlinking:", im)
-        os.remove(im)
