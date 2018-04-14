548547911d41a53b4312b40c246b8455b09429c8
Yannick Hold <yannickhold@gmail.com>
2018-04-13 22:22:30 -0400

    Added reduction script

---
+++ reduce.sh
@@ -0,0 +1,3 @@
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default \
+    -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages \
+    -dCompressFonts=true -r300 -sOutputFile=main_reduced.pdf main.pdf
